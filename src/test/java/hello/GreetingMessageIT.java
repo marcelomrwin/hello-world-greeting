303bbff6c61db4b845328da2fde947be48a2ee18
Marcelo Sales <marcelo.d.sales@gmail.com>
2019-04-19 12:54:24 -0300

    update project

--- src/test/java/hello/GreetingMessageIT.java
+++ src/test/java/hello/GreetingMessageIT.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 
-public class GreetingMessageTest {
+public class GreetingMessageIT {
 
    @Test
    public void testGoodMorning() {
